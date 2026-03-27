29d8f18f883c76ad31d1971b6414ac84439c9781
sumit-dignifyd <sumit.singh@dignifyd.io>
2026-03-27 15:21:45 +0530

    updated comfig files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,14 +3,7 @@ import nextVitals from "eslint-config-next/core-web-vitals";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
 export default eslintConfig;
