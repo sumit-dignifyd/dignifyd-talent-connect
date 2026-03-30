9c1ee9e3fbe4787916c46b27f7f4276152957c8a
sumit-dignifyd <sumit.singh@dignifyd.io>
2026-03-30 16:57:34 +0530

    fix: add Figtree font integration to RootLayout component

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,10 +2,16 @@ import Footer from "@/components/common/Footer";
 import "../styles/globals.css";
 import Navbar from "@/components/common/Navbar";
 import SmoothFollower from "@/components/common/cursors/SmoothFollower";
+import { Figtree } from "next/font/google";
+
+export const figtree = Figtree({
+  subsets: ["latin"],
+  variable: "--font-figtree",
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={figtree.variable}>
       <body className="bg-black text-white">
         <Navbar />
         <SmoothFollower />
