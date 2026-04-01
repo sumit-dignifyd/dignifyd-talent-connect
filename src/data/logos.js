d07da8b6cb4a6ab6adb6b61c4224e571fbd6fc24
sumit-dignifyd <sumit.singh@dignifyd.io>
2026-04-01 14:35:47 +0530

    use just dummy logo till now

--- src/data/logos.js
+++ src/data/logos.js
@@ -1,74 +1,26 @@
-import oppo from "../assets/images/home/oppo.png";
-import minimalist from "../assets/images/home/minimalist.png";
-import maybeline from "../assets/images/home/maybeline.png";
-import mamaearth from "../assets/images/home/mamaearth.png";
-import lotus from "../assets/images/home/lotus.png";
-import livespace from "../assets/images/home/livespace.png";
-import kshm from "../assets/images/home/kshm.png";
-import giva from "../assets/images/home/giva.png";
-import garnier from "../assets/images/home/garnier.png";
-import dove from "../assets/images/home/dove.png";
-import dot_and_key from "../assets/images/home/dot-and-key.png";
-import cerave from "../assets/images/home/cerave.png";
-import boat from "../assets/images/home/boat.webp";
-import avtar from "../assets/images/home/avatar.png";
-import aqualogica from "../assets/images/home/aqualogica.png";
-import snitch from "../assets/images/home/snitch.png";
-import roca from "../assets/images/home/roca.webp";
-import cordelia from "../assets/images/home/cordelia-cruises.webp";
-import ICCPL from "../assets/images/home/ICCPL.webp";
-import aminu from "../assets/images/home/aminu.png";
-import msOffice from "../assets/images/aboutUs/office-365.png";
-import zoho from "../assets/images/aboutUs/zoho.png";
-import hubshot from "../assets/images/aboutUs/hubspot.png";
-import apollo from "../assets/images/aboutUs/apollo.png";
-import lusha from "../assets/images/aboutUs/lusha.png";
-import copilot from "../assets/images/aboutUs/copilot.png";
-import gemini from "../assets/images/aboutUs/gemini.png";
-import perplexity from "../assets/images/aboutUs/perplexity.png";
-import naukri from "../assets/images/aboutUs/naukri.png";
-import monster from "../assets/images/aboutUs/monster.png";
-import indeed from "../assets/images/aboutUs/indeed.png";
-import chatGPT from "../assets/images/aboutUs/chatGPT.png";
-import linkedin from "../assets/images/aboutUs/linkedin.png";
-import salesForce from "../assets/images/aboutUs/salesforce.png";
+import wipro from "../assets/images/home/logos/wipro.png";
 
 export const logos = [
-  oppo,
-  minimalist,
-  maybeline,
-  mamaearth,
-  lotus,
-  livespace,
-  kshm,
-  giva,
-  garnier,
-  dove,
-  dot_and_key,
-  cerave,
-  boat,
-  avtar,
-  aqualogica,
-  snitch,
-  roca,
-  cordelia,
-  ICCPL,
-  aminu,
+  wipro,
+  wipro,
+  wipro,
+  wipro,
+  wipro,
+  wipro,
+  wipro,
+  wipro,
+  wipro,
+  wipro,
+  wipro,
+  wipro,
+  wipro,
+  wipro,
+  wipro,
+  wipro,
+  wipro,
+  wipro,
+  wipro,
+  wipro,
 ];
 
-export const TechSupportLogo = [
-  msOffice,
-  zoho,
-  hubshot,
-  apollo,
-  lusha,
-  copilot,
-  gemini,
-  perplexity,
-  naukri,
-  monster,
-  indeed,
-  chatGPT,
-  linkedin,
-  salesForce,
-];
+export const TechSupportLogo = [];
