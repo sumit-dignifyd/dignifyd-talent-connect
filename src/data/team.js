fd76c1d8ee9ea9e6d99f1f14fa64a9b7bdd12b29
sumit-dignifyd <sumit.singh@dignifyd.io>
2026-04-01 14:38:20 +0530

    changed im[ort path for the team images

--- src/data/team.js
+++ src/data/team.js
@@ -1,16 +1,16 @@
-import pavaan from "../assets/images/aboutUs/pavaan.png";
-import yogesh from "../assets/images/aboutUs/yogesh.png";
-import mandeep from "../assets/images/aboutUs/mandeep.png";
-import sahil from "../assets/images/aboutUs/sahil.png";
-import shivi from "../assets/images/aboutUs/shivi.png";
-import ashish from "../assets/images/aboutUs/ashish.png";
-import krishna from "../assets/images/aboutUs/krishna.jpg";
-import purvi from "../assets/images/aboutUs/purvi.png";
-import rohitesh from "../assets/images/aboutUs/rohitesh.png";
-import shareen from "../assets/images/aboutUs/shareen.png";
-import uday from "../assets/images/aboutUs/uday.png";
-import utsav from "../assets/images/aboutUs/utsav.png";
-import muskan from "../assets/images/aboutUs/muskan.png";
+import pavaan from "../assets/images/aboutUs/team/pavaan.png";
+import yogesh from "../assets/images/aboutUs/team/yogesh.png";
+import mandeep from "../assets/images/aboutUs/team/mandeep.png";
+import sahil from "../assets/images/aboutUs/team/sahil.png";
+import shivi from "../assets/images/aboutUs/team/shivi.png";
+import ashish from "../assets/images/aboutUs/team/ashish.png";
+import krishna from "../assets/images/aboutUs/team/krishna.jpg";
+import purvi from "../assets/images/aboutUs/team/purvi.png";
+import rohitesh from "../assets/images/aboutUs/team/rohitesh.png";
+import shareen from "../assets/images/aboutUs/team/shareen.png";
+import uday from "../assets/images/aboutUs/team/uday.png";
+import utsav from "../assets/images/aboutUs/team/utsav.png";
+import muskan from "../assets/images/aboutUs/team/muskan.png";
 export const leadershipTeam = [
   {
     img: pavaan,
