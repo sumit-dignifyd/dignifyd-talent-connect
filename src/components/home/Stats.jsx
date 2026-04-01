6f10efffd4eb1ac4dc5799c548e184015f6cb11f
sumit-dignifyd <sumit.singh@dignifyd.io>
2026-04-01 14:41:30 +0530

    added icons for the stats cards

--- src/components/home/Stats.jsx
+++ src/components/home/Stats.jsx
@@ -1,20 +1,24 @@
 import SectionHeader from "@/components/common/SectionHeader";
 import InteractiveDots from "../common/cursors/InteractiveDots";
+import { Globe, Handshake, Users } from "lucide-react";
 
 export default function Stats() {
   const stats = [
     {
+      icon: <Handshake size={18} />,
       value: "92%",
       title: "Client Satisfaction",
       desc: "Trusted for delivering consistent quality, reliable performance, and exceptional service excellence every time.",
     },
     {
+      icon: <Users size={18} />,
       value: "95%",
       title: "Employee Retention",
       desc: "We foster supportive and growth-driven environments that keep talent engaged, motivated, and committed.",
     },
     {
-      value: "4000+",
+      icon: <Globe  size={18} />,
+      value: "4,000+",
       title: "Global Placement",
       desc: "Professionals successfully placed across diverse industries, driving growth and long-term success for businesses worldwide.",
     },
@@ -25,7 +29,7 @@ export default function Stats() {
       <InteractiveDots className="absolute inset-0 z-0" />
       <SectionHeader
         badge={"Empowering Global Access"}
-        title={"Custom Workforce Solution That Accelareate Global Growth"}
+        title={"Custom Workforce Solution That Accelerate Global Growth"}
       />
       <div className="mx-auto grid max-w-7xl gap-8 md:grid-cols-3">
         {stats.map((item, index) => (
@@ -36,9 +40,15 @@ export default function Stats() {
             <div className="pointer-events-none absolute inset-0 rounded-2xl shadow-[0_0_40px_rgba(168,85,247,0.08)]"></div>
 
             <div className="relative z-10">
-              <h2 className="mb-4 text-2xl font-semibold text-white">
-                {item.value}
-              </h2>
+              <div className="mb-4 flex items-center gap-3">
+                <div className="flex h-10 w-10 items-center justify-center rounded-full bg-purple-500/20 text-purple-400">
+                  {item.icon}
+                </div>
+
+                <h2 className="text-2xl font-semibold text-white">
+                  {item.value}
+                </h2>
+              </div>
 
               <h3 className="mb-3 text-lg font-medium text-white">
                 {item.title}
